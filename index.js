a95e47cd444c24b2f3f78870b4656fa99212f8bd
HotMonkeyWings <devsony52@gmail.com>
2022-03-02 06:55:50 +0530

    Finished quest

--- index.js
+++ index.js
@@ -1,25 +1,120 @@
-// First import the solana web3 library
-const web3 = require("@solana/web3.js");
+// Display & Input Modules
+const chalk = require('chalk');
+const figlet = require('figlet');
+const inquirer = require('inquirer');
 
-// Establish a connection, here with the devnet
-// Second parameter specifies the commitment level
-// "confirmed" => node will query most recent block voted by supermajority of cluster. 
-//                  - It will incorporate votes from gossip and replay.
-//                  - It doesn't count votes on descendants of block, only direct votes on the block
-//                  - The confirmation level upholds "optimistic confirmation" guaranttees.
-//
-// "finalized" => node will query the most recent block confirmed by supermajority of the cluster
-//                as having reached the maximum lockout => CLUSTER RECOGNIZED BLOCK AS FINALIZED
-//
-// "processed" => Node will query its most recent block. Block may be still skipped by cluster.
-//
-// Safety: finalized > confirmed > processed
-// Speed: processed > confirmed > finalized
-// => Confirmed is best & recommended
-const connection = new web3.Connection(web3.clusterApiUrl("devnet"), "confirmed")
-// console.log(connection);
+// Utility and Helper functions
+const web3 = require('@solana/web3.js');
+const {
+    getReturnAmount,
+    randomNumber
+} = require('./helper');
+const {
+    getWalletBalance,
+    transferSOL,
+    airDropSol
+} = require('./solana');
+const fs = require('fs');
 
-// Player's wallet, which we'll be generating for now.
-const userWallet = web3.Keypair.generate();
+const questions = [{
+        type: 'input',
+        name: 'SOLamt',
+        message: 'What is the amount of SOL you want to stake?',
+        default () {
+            return 1;
+        },
+    },
+    {
+        type: 'input',
+        name: 'ratio',
+        message: 'What is the ratio of your staking?',
+        default () {
+            return '1:1';
+        },
+    },
+];
 
-console.log(userWallet);
+let fromWalletPath = './solana-wallet/keypair.json';
+let fromWallet;
+
+if (!fs.existsSync(fromWalletPath)) {
+    fromWallet = web3.Keypair.generate();
+} else {
+    fromWallet = web3.Keypair.fromSecretKey(
+        Uint8Array.from(JSON.parse(fs.readFileSync(fromWalletPath)))
+    );
+}
+
+let toWalletPath = './solana-wallet/keypair2.json';
+let toWallet;
+
+if (!fs.existsSync(toWalletPath)) {
+    toWallet = web3.Keypair.generate();
+} else {
+    toWallet = web3.Keypair.fromSecretKey(
+        Uint8Array.from(JSON.parse(fs.readFileSync(toWalletPath)))
+    );
+}
+
+const gameExecution = async () => {
+    figlet('SOL Stake', (err, data) => {
+        if (err) {
+            console.dir(err);
+            return;
+        }
+        console.log(chalk.green(data));
+        console.log(chalk.yellow('The max bidding amount is 2.5 SOL here'));
+        inquirer.prompt(questions).then((answers) => {
+            let SOLamt = answers['SOLamt'];
+            let ratio = answers['ratio'];
+            let returnAmount = getReturnAmount(SOLamt, ratio);
+            console.log(
+                'You need to pay',
+                chalk.green(answers['SOLamt']),
+                'to move forward'
+            );
+            transferSOL(toWallet, fromWallet, SOLamt).then((signature) => {
+                console.log(
+                    chalk.green(
+                        'You will get',
+                        returnAmount,
+                        'if guessing the number correctly'
+                    )
+                );
+                inquirer
+                    .prompt([{
+                        type: 'input',
+                        name: 'guess',
+                        message: 'Guess a random number from 1 to 5 (both 1, 5 included)',
+                        default () {
+                            return 1;
+                        },
+                    }, ])
+                    .then((answers) => {
+                        let randomGuess = randomNumber(1, 5);
+                        console.log(
+                            'Signature of payment for playing the game',
+                            chalk.green(signature)
+                        );
+                        if (randomGuess == answers['guess']) {
+                            console.log(
+                                chalk.green('Your guess is absolutely correct')
+                            );
+                            airDropSol(
+                                fromWallet.publicKey,
+                                returnAmount - SOLamt
+                            ).then(
+                                transferSOL(fromWallet, toWallet, returnAmount).then((winSign) => {
+                                    console.log("Here is the price signature", chalk.green(winSign))
+                                })
+                            );
+                        } else {
+                            console.log(chalk.yellow('Better luck next time'));
+                        }
+                    });
+            });
+        });
+    });
+};
+
+gameExecution();
